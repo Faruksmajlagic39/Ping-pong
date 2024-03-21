128b139053364e6b040405aa6b634850b393adb9
Faruksmajlagic39 <162631251+Faruksmajlagic39@users.noreply.github.com>
2024-03-21 20:50:51 +0100

    Add files via upload
    v2

--- Pingpong.py
+++ Pingpong.py
@@ -1,5 +1,36 @@
 from pygame import*
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #e.g. 55,55 - parameters
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width-80:
+            self.rect.y += self.speed
+
+        def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_S] and self.rect.y < win_width-80:
+            self.rect.y += self.speed
+
+
+
 back = (200, 255, 255)
 win_width = 600
 win_height = 500
@@ -10,6 +41,8 @@ window.fill(back)
 game = True
 clock = time.Clock()
 FPS = 60
+
+racket1 = Player()
 
 while game:
     for e in event.get():
